61d95e7d6031189a31c3b0e5bc851a57b152b7fc
Cesar Gonzalez <cesargonzalez390@gmail.com>
2024-09-24 19:30:41 -0500

    feat: add main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,37 @@
-import Button from '@mui/material/Button';
+import { Container, Box, Typography, Grid2 } from "@mui/material";
+import { BannerMain } from "./ui/banner";
+import { fetchCategory } from "./utils/categories";
+import CategoriesComponent from "./ui/subcategory-menu";
 
-export default function Home() {
+export const metadata = {
+  title: "Corporacion Luana",
+  description: "Tienda de corporacion luana",
+};
+
+export default async function Home() {
+  const categories = await fetchCategory();
+
+  // filtro de categorias activas
+  const activeCategories = categories.filter((category) => category.is_active);
+
   return (
-    <main>
-      <p>Inicio</p>
-      <Button variant="contained">Hello world</Button>
-    </main>
+    <Container maxWidth="xl">
+      <Box>
+        <BannerMain />
+      </Box>
+      <Box component="section">
+        <Box>
+          <Typography
+            variant="h4"
+            sx={{ textAlign: "center", fontWeight: "bold" }}
+          >
+            Nuestras categorías
+          </Typography>
+          <Box>
+            <CategoriesComponent categories={activeCategories}/>
+          </Box>
+        </Box>
+      </Box>
+    </Container>
   );
 }
