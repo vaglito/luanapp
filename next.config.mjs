fd01e2c8b091ccf4813b719abc79c79719bc30f8
Cesar Gonzalez <cesargonzalez390@gmail.com>
2024-09-24 19:04:08 -0500

    feat: :art: Add swiper library

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'luanatech.pe'
+            }
+        ]
+    }
+};
 
 export default nextConfig;
