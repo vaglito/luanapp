6cd7f6fb27dc26a9bf3194f880239f1ae0e74756
Cesar Gonzalez <cesargonzalez390@gmail.com>
2025-03-19 20:15:09 -0500

    feat: add product list fetching function with error handling

--- src/app/services/products.ts
+++ src/app/services/products.ts
@@ -101,4 +101,52 @@ export const getProductDetail = async (slug: string): Promise<Product> => {
     }
 }
 
+/**
+ * Fetches the list of products based on the provided category and subcategory slugs.
+ *
+ * @param {string} categorySlug - The slug of the category.
+ * @param {string} subcategorySlug - The slug of the subcategory.
+ * @param {number} page - The page number to fetch.
+ * @returns {Promise<ResponseProducts>} - A promise that resolves to the list of products.
+ * @throws {Error} - Throws an error if the response status is 404 (Not Found), 500 (Internal Server Error), or any other unexpected status.
+ *
+ * @example
+ * getProductList('category-slug', 'subcategory-slug', 1)
+ *   .then(products => console.log(products))
+ *   .catch(error => console.error(error));
+ */
+export const getProductList = async (brandSlug:string, categorySlug: string, subcategorySlug: string, page: number): Promise<ResponseProducts> => {
+    try {
+        const response = await fetch(`${apiUrl}/api/v2.0/products/list/?brands=${brandSlug}?category=${categorySlug}/?subcategory=${subcategorySlug}/?page=${page}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        // Check if the response is ok
+        if (!response.ok) {
+            switch (response.status) {
+                case 404:
+                    throw new Error('The requested resource could not be found. Not Found 404');
+                case 500:
+                    throw new Error('Internal Server Error 500.');
+                default:
+                    throw new Error(`Unexpected error: ${response.status}`);
+            }
+        }
+
+        const data: ResponseProducts = await response.json();
+        return data;
+
+    } catch (error) {
+        console.error('Error fetching product list: ', error);
+        return {
+            count: 0,
+            next: null,
+            previous: null,
+            results: []
+        }
+    }
+}
 
