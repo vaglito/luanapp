09916a4de465eb3552fd7c958d43fca6de4c5b1e
Cesar Gonzalez <cesargonzalez390@gmail.com>
2025-03-19 19:48:33 -0500

    feat: implement product fetching services with error handling

---
+++ src/app/services/products.ts
@@ -0,0 +1,104 @@
+import { ResponseProducts, Product } from "../types/v2/products-type";
+
+const apiUrl = process.env.API_URL
+
+/**
+ * Fetches the list of recent products from the API.
+ *
+ * @returns {Promise<ResponseProducts>} A promise that resolves to the list of recent products.
+ *
+ * @throws {Error} Throws an error if the response status is 404 (Not Found), 500 (Internal Server Error), 
+ * or any other unexpected status.
+ *
+ * @example
+ * getNewProductList().then(products => {
+ *   console.log(products);
+ * }).catch(error => {
+ *   console.error(error);
+ * });
+ */
+export const getNewProductList = async (): Promise<ResponseProducts> => {
+    try {
+        const response = await fetch(`${apiUrl}/api/v2.0/products/recent/`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        // Check if the response is ok
+        if (!response.ok) {
+            switch (response.status) {
+                case 404:
+                    throw new Error('The requested resource could not be found. Not Found 404');
+                case 500:
+                    throw new Error('Internal Server Error 500.');
+                default:
+                    throw new Error(`Unexpected error: ${response.status}`);
+            }
+        }
+
+        const data: ResponseProducts = await response.json();
+        return data;
+
+    } catch (error) {
+        console.error('Error fetching new products: ', error);
+        return {
+            count: 0,
+            next: null,
+            previous: null,
+            results: []
+        }
+    }
+}
+
+/**
+ * Fetches the details of a product based on the provided slug.
+ *
+ * @param {string} slug - The unique identifier for the product.
+ * @returns {Promise<Product>} - A promise that resolves to the product details.
+ * @throws {Error} - Throws an error if the response status is 404 (Not Found), 500 (Internal Server Error), or any other unexpected status.
+ *
+ * @example
+ * getProductDetail('product-slug')
+ *   .then(product => console.log(product))
+ *   .catch(error => console.error(error));
+ */
+export const getProductDetail = async (slug: string): Promise<Product> => {
+    try {
+        const response = await fetch(`${apiUrl}/api/v2.0/products/get/${slug}/`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        // Check if the response is ok
+        if (!response.ok) {
+            switch (response.status) {
+                case 404:
+                    throw new Error('The requested resource could not be found. Not Found 404');
+                case 500:
+                    throw new Error('Internal Server Error 500.');
+                default:
+                    throw new Error(`Unexpected error: ${response.status}`);
+            }
+        }
+
+        const data: Product = await response.json();
+        return data;
+
+    } catch (error) {
+        console.error('Error fetching product detail: ', error);
+        return {
+            pk: 0,
+            is_active: false,
+            slug: '',
+            keywords: '',
+            sopprod: [],
+            productimage_set: []
+        }
+    }
+}
+
+
