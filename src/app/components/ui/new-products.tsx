cf7a441876101fdf1a68110a8999b01e36074288
Cesar Gonzalez <cesargonzalez390@gmail.com>
2025-03-26 14:53:51 -0500

    feat: add NewProducts component for displaying newly added products

---
+++ src/app/components/ui/new-products.tsx
@@ -0,0 +1,20 @@
+import { Typography, Box } from "@mui/material";
+import { SliderProduct } from "../product/slider-product";
+import { Product } from "@/app/types/v2/products-type";
+
+export const NewProducts = ({ products }: { products: Product[] }) => {
+  return (
+    <Box>
+      <Box sx={{ display: "flex", justifyContent: "space-between", alignItems: "center", marginTop: "1.5rem" }}>
+        <Typography variant="h1" sx={{
+            fontSize: "2.0rem",
+            fontWeight: "bold",
+            color: "#333",
+        }}>Nuevos Ingresos</Typography>
+      </Box>
+      <Box>
+        <SliderProduct products={products} />
+      </Box>
+    </Box>
+  );
+};
