aba2e1b3edb5f227cee7ff89be695fe66505f758
Cesar Gonzalez <cesargonzalez390@gmail.com>
2025-04-01 13:19:59 -0500

    feat: enhance CardCategory component with subcategory menu and tooltip functionality

--- src/app/components/ui/categoryList/card-category.tsx
+++ src/app/components/ui/categoryList/card-category.tsx
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import Image from "next/image";
-import { Box, Typography } from "@mui/material";
-import { Categorys } from "@/app/types/v2/categorys-type";
+import Link from "next/link";
+import { Box, Typography, Menu, MenuItem } from "@mui/material";
+import { Categorys, Subcategory } from "@/app/types/v2/categorys-type";
 import { styled } from "@mui/material/styles";
 import Tooltip, { TooltipProps, tooltipClasses } from "@mui/material/Tooltip";
 
@@ -16,51 +18,116 @@ const BootstrapTooltip = styled(({ className, ...props }: TooltipProps) => (
 }));
 
 export const CardCategory = ({ category }: { category: Categorys }) => {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const [open, setOpen] = useState(false);
+  const [selectedSubcategories, setSelectedSubcategories] = useState<
+    Subcategory[]
+  >([]);
+  const [selectedCategorySlug, setSelectedCategorySlug] = useState<string>("");
+
+  const handleClick = (
+    event: React.MouseEvent<HTMLImageElement>,
+    subcategories: Subcategory[],
+    categorySlug: string
+  ) => {
+    setAnchorEl(event.currentTarget);
+    setSelectedSubcategories(subcategories);
+    setSelectedCategorySlug(categorySlug);
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+    setOpen(false);
+  };
+
   return (
-    <BootstrapTooltip title={category.soplinea.nom_line}>
-      <Box
-        sx={{
-          bgcolor: "#e1e1e1",
-          borderRadius: "50%",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          padding: 2,
-          width: { xs: 110, sm: 120, md: 190 }, // Tamaño ajustable según el viewport
-          height: { xs: 110, sm: 120, md: 190 },
-          overflow: "hidden", // Evita que la imagen sobresalga del borde redondo
-          mx: "auto", // Centra el contenedor
+    <>
+      <BootstrapTooltip title={category.soplinea.nom_line}>
+        <Box
+          sx={{
+            bgcolor: "#e1e1e1",
+            borderRadius: "50%",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            padding: 2,
+            width: { xs: 110, sm: 120, md: 190 }, // Tamaño ajustable según el viewport
+            height: { xs: 110, sm: 120, md: 190 },
+            overflow: "hidden", // Evita que la imagen sobresalga del borde redondo
+            mx: "auto", // Centra el contenedor
+          }}
+        >
+          {category.image ? (
+            <Image
+              src={category.image}
+              alt={`Imagen de ${category.soplinea.nom_line ?? "categoría"}`}
+              sizes="100vw"
+              width={250}
+              height={250}
+              className="transition duration-150 ease-in-out hover:scale-110"
+              style={{
+                width: "100%", // Ajustar a contenedor
+                height: "100%", // Ajustar a contenedor
+                objectFit: "cover", // Evitar distorsión
+                borderRadius: "50%", // Hacer la imagen redonda
+                cursor: "pointer",
+              }}
+              aria-label={`Ver subcategorias de ${
+                category.soplinea.nom_line ?? "categoria"
+              }`}
+              id={`categoria-${category.pk}`}
+              aria-controls={open ? "demo-positioned-menu" : undefined}
+              aria-haspopup="true"
+              aria-expanded={open ? "true" : undefined}
+              onClick={(event: React.MouseEvent<HTMLImageElement>) =>
+                handleClick(event, category.subcategory, category.slug)
+              }
+            />
+          ) : (
+            <Typography
+              variant="body1"
+              sx={{
+                fontSize: "1.0rem",
+                textAlign: "center",
+                color: "#333",
+              }}
+            >
+              {category.soplinea.nom_line}
+            </Typography>
+          )}
+        </Box>
+      </BootstrapTooltip>
+      <Menu
+        id="demo-positioned-menu"
+        aria-labelledby="demo-positioned-button"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "left",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "left",
         }}
       >
-        {category.image ? (
-          <Image
-            src={category.image}
-            alt={`Imagen de ${category.soplinea.nom_line ?? "categoría"}`}
-            sizes="100vw"
-            width={250}
-            height={250}
-            className="transition duration-150 ease-in-out hover:scale-110"
-            style={{
-              width: "100%", // Ajustar a contenedor
-              height: "100%", // Ajustar a contenedor
-              objectFit: "cover", // Evitar distorsión
-              borderRadius: "50%", // Hacer la imagen redonda
-              cursor: "pointer",
-            }}
-          />
-        ) : (
-          <Typography
-            variant="body1"
-            sx={{
-              fontSize: "1.0rem",
-              textAlign: "center",
-              color: "#333",
-            }}
-          >
-            {category.soplinea.nom_line}
-          </Typography>
-        )}
-      </Box>
-    </BootstrapTooltip>
+        {selectedSubcategories.map((subcategory) => (
+          <>
+            {subcategory.is_active && (
+              <Link
+                href={`/productos/${selectedCategorySlug}/${subcategory.slug}`}
+                key={subcategory.sopsub1.cod_sub1}
+              >
+                <MenuItem onClick={handleClose}>
+                  {subcategory.sopsub1.nom_sub1}
+                </MenuItem>
+              </Link>
+            )}
+          </>
+        ))}
+      </Menu>
+    </>
   );
 };
