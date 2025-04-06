245f62b33a79bac52b96cad3fb663d92bb217d6b
Cesar Gonzalez <cesargonzalez390@gmail.com>
2025-04-05 20:02:09 -0500

    feat: refactor search page to use new product search service and improve error handling

--- src/app/(site)/buscar/page.tsx
+++ src/app/(site)/buscar/page.tsx
@@ -1,84 +1,86 @@
-import { Suspense } from "react";
-import { Container, Box, Typography, Button, Grid2 } from "@mui/material";
-import { fetchProductSearch } from "@/app/utils/products";
+import { getProductSearch } from "@/app/services/products";
+import { ResponseProducts } from "@/app/types/v2/products-type";
+import { Box, Container, Typography, Grid2, Button } from "@mui/material";
 import ErrorIcon from "@mui/icons-material/Error";
 import Link from "next/link";
-import { Filters } from "@/app/ui/filters/filters";
-import { FiltersDesktop } from "@/app/ui/filters/filters-desktop";
-import { SearchProduct } from "@/app/ui/product/search-product/search-products";
+import { GridProduct } from "@/app/components/product/grid-product";
 
-export const generateMetadata = ({
-  searchParams,
-}: {
-  searchParams: { q?: string };
-}) => {
-  const query = searchParams.q || "";
-
-  return {
-    title: query
-      ? `Buscando ${query} | Corporación Luana`
-      : "Búsqueda | Corporación Luana",
-    description: query
-      ? `Estás buscando ${query} en Corporación Luana. Encuentra los mejores productos disponibles.`
-      : "Realiza una búsqueda en Corporación Luana para encontrar productos.",
-  };
-};
-
-export const revalidate = 0;
-
-export default async function Page({
-  searchParams,
-}: {
+interface searchParamsProps {
   searchParams?: {
     query?: string;
     page?: string;
     marca?: string;
     subcategoria?: string;
   };
-}) {
+}
+
+export default async function SearchPage({ searchParams }: searchParamsProps) {
   const query = searchParams?.query || "";
-  // Solo mantener marca y subcategoria si la búsqueda es la misma
   const keepFilters = searchParams?.query && query === searchParams.query;
   const marca = keepFilters ? searchParams?.marca || "" : "";
   const subcategoria = keepFilters ? searchParams?.subcategoria || "" : "";
-
   let currentPage = Number(searchParams?.page) || 1;
 
-  // Manejo de errores en la API
-  let searchProduct;
+  let searchProduct: ResponseProducts | undefined;
+
   try {
-    searchProduct = await fetchProductSearch(
-      query,
-      marca,
-      subcategoria,
-      currentPage
-    );
+    searchProduct = await getProductSearch({
+      query: query,
+      brandSlug: marca,
+      subcategorySlug: subcategoria,
+      page: currentPage,
+    });
   } catch (error) {
     return (
       <Container maxWidth="xl">
         <Box className="h-screen" sx={{ marginY: 4 }}>
           <Box
             sx={{
-              marginY: 4,
-              padding: "20px",
-              borderRadius: "12px",
-              boxShadow: "0 4px 15px rgba(0, 0, 0, 0.1)",
-              backgroundColor: "#ffffff",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "center",
+              height: "100%",
             }}
           >
-            <Typography variant="h4" gutterBottom>
-              {query
-                ? `Resultados para: ${query}`
-                : "Introduce un término de búsqueda"}
+            <ErrorIcon color="error" sx={{ fontSize: 50 }} />
+            <Typography variant="h5" component="h2" gutterBottom>
+              Ocurrió un error al buscar productos.
             </Typography>
+            <Link href="/">
+              <Button variant="contained" color="primary">
+                Volver a la página principal
+              </Button>
+            </Link>
           </Box>
-          <Box sx={{ display: "flex", gap: 1 }}>
-            <ErrorIcon color="error" />
-            <Typography variant="h3" color="error">
-              ¡Lo sentimos!
+        </Box>
+      </Container>
+    );
+  }
+
+  if (!searchProduct) {
+    return (
+      <Container maxWidth="xl">
+        <Box className="h-screen" sx={{ marginY: 4 }}>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "center",
+              height: "100%",
+            }}
+          >
+            <ErrorIcon color="error" sx={{ fontSize: 50 }} />
+            <Typography variant="h5" component="h2" gutterBottom>
+              No se encontraron resultados para "{query}".
             </Typography>
+            <Link href="/">
+              <Button variant="contained" color="primary">
+                Volver a la página principal
+              </Button>
+            </Link>
           </Box>
-          <Typography variant="h5">No se encontró ningún producto.</Typography>
         </Box>
       </Container>
     );
@@ -90,72 +92,16 @@ export default async function Page({
   }
 
   return (
-    <Container maxWidth="xl">
-      <Box
-        sx={{
-          marginY: 4,
-          padding: "20px",
-          borderRadius: "12px",
-          boxShadow: "0 4px 15px rgba(0, 0, 0, 0.1)",
-          backgroundColor: "#ffffff",
-        }}
-      >
+    <Box>
+      <Box sx={{ display: "flex", alignItems: "center", marginY: 2, justifyContent: "space-between" }}>
         <Typography variant="h4" gutterBottom>
-          {query
-            ? `Resultados para: ${query}`
-            : "Introduce un término de búsqueda"}
+          Resultados para: {query}
         </Typography>
-        <Typography>Productos encontrados: {searchProduct?.count}</Typography>
+        <Typography>{searchProduct.count} productos encontrados</Typography>
       </Box>
-
-      <Box sx={{ marginY: 4 }}>
-        <Suspense fallback={<Typography variant="h5">Cargando...</Typography>}>
-          {searchProduct && searchProduct.results.length > 0 ? (
-            <Grid2 container spacing={3}>
-              <Grid2 size={{ xs: 12, sm: 12, md: 3, lg: 3, xl: 3 }}>
-                <Box sx={{ display: { xs: "none", md: "flex" }, flexDirection: "column"}}>
-                  <FiltersDesktop product={searchProduct} />
-                </Box>
-                <Box sx={{ display: { md: "none" } }}>
-                  <Filters product={searchProduct} />
-                </Box>
-              </Grid2>
-              <Grid2 size={{ xs: 12, sm: 12, md: 9, lg: 9, xl: 9 }}>
-                <SearchProduct searchProduct={searchProduct} />
-              </Grid2>
-            </Grid2>
-          ) : (
-            <Typography variant="body1">
-              {query
-                ? "No se encontraron productos para tu búsqueda."
-                : "Introduce un término para buscar productos."}
-            </Typography>
-          )}
-        </Suspense>
+      <Box>
+        <GridProduct products={searchProduct.results}/>
       </Box>
-      {/* Paginación con URLSearchParams */}
-      <Box display="flex" justifyContent="center" mt={3}>
-        {Array.from({ length: totalPages }, (_, index) => {
-          const params = new URLSearchParams();
-          if (query) params.set("query", query);
-          if (marca) params.set("marca", marca);
-          if (subcategoria) params.set("subcategoria", subcategoria);
-          params.set("page", (index + 1).toString());
-
-          return (
-            <Button
-              key={index + 1}
-              variant="outlined"
-              color={currentPage === index + 1 ? "primary" : "secondary"}
-              component={Link}
-              href={`/buscar?${params.toString()}`}
-              sx={{ mx: 0.5 }}
-            >
-              {index + 1}
-            </Button>
-          );
-        })}
-      </Box>
-    </Container>
+    </Box>
   );
 }
