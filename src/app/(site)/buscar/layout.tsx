cd3df890e22cacbe6d0ff27f12879328ca14afb4
Cesar Gonzalez <cesargonzalez390@gmail.com>
2025-04-07 19:21:31 -0500

    feat: add Suspense to SearchLayout for improved loading experience

--- src/app/(site)/buscar/layout.tsx
+++ src/app/(site)/buscar/layout.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import { Filter } from "@/app/components/product/search/Filter";
 import { Box, Container, Typography, Grid2 } from "@mui/material";
 
@@ -11,7 +12,9 @@ export default function SearchLayout({
       <Grid2 container spacing={3} sx={{ marginY: 4 }}>
         <Grid2 size={{ xs: 12, sm: 12, md: 3, lg: 3, xl: 3 }}>
           <Box sx={{ padding: 2, borderRadius: 2, backgroundColor: "#e7e7e7" }}>
+            <Suspense fallback={<Typography variant="h6">Cargando filtros...</Typography>}>
             <Filter />
+            </Suspense>
           </Box>
         </Grid2>
         <Grid2 size={{ xs: 12, sm: 12, md: 9, lg: 9, xl: 9 }}>{children}</Grid2>
