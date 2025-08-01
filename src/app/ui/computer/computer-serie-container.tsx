ee2660a10d2d8063057e433fa3f183212e35e28d
Cesar Gonzalez <cesargonzalez390@gmail.com>
2025-08-01 06:34:01 -0500

    feat: add ComputerSerieContainer component to fetch and display computers by series

---
+++ src/app/ui/computer/computer-serie-container.tsx
@@ -0,0 +1,21 @@
+import { Box, Typography } from "@mui/material";
+import { fetchComputer } from "@/app/services/computer";
+import { ComputerContainer } from "./computer-container";
+
+export async function ComputerSerieContainer({ serie }: { serie: number }) {
+  const computers = await fetchComputer(serie);
+
+  if (!computers || computers.count == 0) {
+    return (
+      <Box>
+        <Typography>No hay computadoras</Typography>
+      </Box>
+    );
+  }
+
+  return (
+    <Box>
+        <ComputerContainer computers={computers} />
+    </Box>
+  );
+}
