3b973238a37e55d5f816ffe1f88ef354e5fbef47
Cesar Gonzalez <cesargonzalez390@gmail.com>
2025-08-12 02:31:25 -0500

    Improves product slider with navigation and autoplay
    Enhances the product slider component by adding navigation buttons for manual control and autoplay for automatic sliding.
    Also adjusts the styling and spacing of the slider and its items for a better user experience.

--- src/app/components/product/slider-product.tsx
+++ src/app/components/product/slider-product.tsx
@@ -1,13 +1,12 @@
 "use client";
-// Import Swiper styles
 import "swiper/css";
-import "swiper/css/pagination";
-import { Box } from "@mui/material";
-
+import "swiper/css/navigation";
+import { Box, IconButton } from "@mui/material";
 import { Swiper, SwiperSlide } from "swiper/react";
-// import required modules
-import { Pagination } from "swiper/modules";
+import { Navigation, Autoplay } from "swiper/modules";
 import { Product } from "@/app/types/v2/products-type";
+import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
 interface SliderProductProps {
   products: Product[];
@@ -16,35 +15,62 @@ interface SliderProductProps {
 
 export const SliderProduct = ({ products, Component }: SliderProductProps) => {
   return (
-    <Box>
+    <Box sx={{ position: "relative", marginTop: 2 }}>
+      {/* Botón anterior */}
+      <IconButton
+        className="custom-prev"
+        color="primary"
+        sx={{
+          position: "absolute",
+          top: "50%",
+          left: -50,
+          zIndex: 10,
+          boxShadow: 1,
+        }}
+      >
+        <ArrowBackIosNewIcon fontSize="small" />
+      </IconButton>
+
+      {/* Botón siguiente */}
+      <IconButton
+        className="custom-next"
+        color="primary"
+        sx={{
+          position: "absolute",
+          top: "50%",
+          right: -50,
+          zIndex: 10,
+          boxShadow: 1,
+        }}
+      >
+        <ArrowForwardIosIcon fontSize="small" />
+      </IconButton>
+
       <Swiper
-        style={{ paddingLeft: 10, paddingRight: 10 }}
-        modules={[Pagination]}
+        modules={[Navigation, Autoplay]}
+        loop={true}
+        autoplay={{
+          delay: 3500,
+          disableOnInteraction: false,
+        }}
+        navigation={{
+          prevEl: ".custom-prev",
+          nextEl: ".custom-next",
+        }}
         slidesPerView={2}
-        spaceBetween={10} // 👈 default para móviles
-        pagination={{ clickable: true }}
+        spaceBetween={20}
         breakpoints={{
-          640: {
-            slidesPerView: 2,
-            spaceBetween: 20,
-          },
-          768: {
-            slidesPerView: 3,
-            spaceBetween: 20,
-          },
-          1024: {
-            slidesPerView: 4,
-            spaceBetween: 24, // puedes ajustarlo para pantallas grandes
-          },
-          1280: {
-            slidesPerView: 4,
-            spaceBetween: 24,
-          },
+          640: { slidesPerView: 2, spaceBetween: 20 },
+          768: { slidesPerView: 3, spaceBetween: 20 },
+          1024: { slidesPerView: 4, spaceBetween: 24 },
+          1280: { slidesPerView: 4, spaceBetween: 24 },
         }}
       >
         {products.map((product) => (
           <SwiperSlide key={product.pk}>
-            <Component product={product} />
+            <Box sx={{ paddingY: 4, paddingX: { xs: 1, sm: 1, md: 2, lg: 2 } }}>
+              <Component product={product} />
+            </Box>
           </SwiperSlide>
         ))}
       </Swiper>
