5907f197542fddee9f7b5ddb2a4f0597415e8067
Cesar Gonzalez <cesargonzalez390@gmail.com>
2025-08-12 02:31:40 -0500

    Cleans up GridProduct component
    Removes unused imports and adds a line break for better readability.

--- src/app/components/product/grid-product.tsx
+++ src/app/components/product/grid-product.tsx
@@ -1,15 +1,16 @@
-import { Box, Grid2, Typography } from "@mui/material";
+import { Grid2 } from "@mui/material";
 import { CardProduct } from "@/app/components/product/card-product";
 import { Product } from "@/app/types/v2/products-type";
 
+
 export const GridProduct = ({ products }: { products: Product[] }) => {
-    return (
-        <Grid2 container spacing={2}>
-            {products.map((product) => (
-                <Grid2 size={{ xs: 6, sm: 4, md: 3}} key={product.pk}>
-                    <CardProduct product={product} />
-                </Grid2>
-            ))}
+  return (
+    <Grid2 container spacing={2}>
+      {products.map((product) => (
+        <Grid2 size={{ xs: 6, sm: 4, md: 3 }} key={product.pk} >
+          <CardProduct product={product} />
         </Grid2>
-    )
-}
+      ))}
+    </Grid2>
+  );
+};
