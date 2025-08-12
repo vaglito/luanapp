627068c45faa1f852b4ba9a64edd521d89b33384
Cesar Gonzalez <cesargonzalez390@gmail.com>
2025-08-12 00:42:01 -0500

    Refactors product card component
    Improves the product card component by extracting image and stock information into separate components.
    
    This change enhances code readability and maintainability by modularizing the card's structure. It also enhances the visual appearance and user experience with minor styling adjustments and adds a "Ver más" button for quick navigation to the product detail page.

--- src/app/components/product/card-product.tsx
+++ src/app/components/product/card-product.tsx
@@ -1,27 +1,31 @@
 "use client";
-import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 import { Product } from "@/app/types/v2/products-type";
-import { Box, Typography, IconButton, Tooltip } from "@mui/material";
-import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
+import { Box, Typography, Button } from "@mui/material";
+import { CardImage } from "./CardProduct/card-imagen";
+import { CardStock } from "./CardProduct/card-stock";
+
 import { PriceCard } from "./CardProduct/price-card";
 
 export const CardProduct = ({ product }: { product: Product }) => {
+  const router = useRouter();
   return (
     <Box
       sx={{
         display: "flex",
+        marginTop: 4,
+        marginBottom: 8,
         flexDirection: "column",
         position: "relative",
         width: "100%",
-        maxWidth: "250px", // importante para responsive dentro del slider
-        height: "auto",
+        height: "100%",
         borderRadius: 3,
         overflow: "hidden",
-        marginY: 5,
+        boxShadow: "0 0 10px rgba(0, 0, 0, 0.1)",
         transition: "box-shadow 0.3s ease-in-out",
         "&:hover": {
-          boxShadow: "0 0 10px rgba(0, 0, 0, 0.1)",
+          boxShadow: "0px 0px 20px rgb(203, 182, 214)",
         },
         "&:hover .cart-icon": {
           opacity: 1,
@@ -29,40 +33,11 @@ export const CardProduct = ({ product }: { product: Product }) => {
       }}
     >
       {/* Contenedor de la imagen */}
-      <Box
-        sx={{
-          position: "relative",
-          width: "100%",
-          aspectRatio: "1 / 1", // asegura imagen cuadrada y responsiva
-          overflow: "hidden",
-        }}
-      >
-        <Image
-          src={product.productimage_set[0].images}
-          alt={product.sopprod.nom_prod}
-          width={250}
-          height={250}
-        />
-
-        {/* Icono del carrito al hacer hover */}
-        <Tooltip title="Agregar al carrito" placement="top">
-          <IconButton
-            aria-label="add"
-            className="cart-icon"
-            color="primary"
-            sx={{
-              position: "absolute",
-              top: "50%",
-              right: "8px",
-              transform: "translateY(-50%)",
-              opacity: 0,
-              transition: "opacity 0.3s ease",
-            }}
-          >
-            <AddShoppingCartIcon />
-          </IconButton>
-        </Tooltip>
-      </Box>
+      <CardImage
+        productimage_set={product.productimage_set}
+        title={product.sopprod.nom_prod}
+        slug={product.slug}
+      />
 
       {/* Información del producto */}
       <Link href={`/productos/detalle/${product.slug}`}>
@@ -72,35 +47,45 @@ export const CardProduct = ({ product }: { product: Product }) => {
             sx={{
               lineHeight: 1.2,
               fontWeight: 600,
+              color: "#545454",
               fontSize: { xs: "0.85rem", sm: "1rem" },
               display: "-webkit-box",
               WebkitBoxOrient: "vertical",
+              textAlign: "justify",
               overflow: "hidden",
               WebkitLineClamp: 3,
               textOverflow: "ellipsis",
               transition: "color 0.2s",
               "&:hover": {
-                color: "#828282",
+                color: "primary.main",
               },
             }}
           >
-            {product.sopprod.nom_prod.toUpperCase()}
+            {product.sopprod.nom_prod}
           </Typography>
           <PriceCard price={product.sopprod.cod_prod_relation_precios[0]} />
-          <Box
-            sx={{
-              display: "flex",
-              justifyContent: "space-between",
-              mt: 1,
-              flexWrap: "wrap", // permite que baje si no cabe
-              gap: "0.25rem",
-            }}
-          >
-            <Typography>Stock: {product.sopprod.stock_index}</Typography>
-            <Typography>Codigo: {product.sopprod.pk}</Typography>
-          </Box>
+          <CardStock
+            stock={product.sopprod.stock_index}
+            cod={product.sopprod.pk}
+          />
         </Box>
       </Link>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          pb: 2,
+          alignItems: "end",
+        }}
+      >
+        <Button
+          onClick={() => router.push(`/productos/detalle/${product.slug}`)}
+          variant="outlined"
+          sx={{ borderRadius: 5, width: "70%", textTransform: "capitalize" }}
+        >
+          Ver más
+        </Button>
+      </Box>
     </Box>
   );
 };
