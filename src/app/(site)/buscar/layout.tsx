09d9d0784853192d5f258b9acb6b07aa12ddcf16
Cesar Gonzalez <cesargonzalez390@gmail.com>
2025-09-19 05:23:47 -0500

    refactor: :fire: remove properties the block

--- src/app/(site)/buscar/layout.tsx
+++ src/app/(site)/buscar/layout.tsx
@@ -9,16 +9,7 @@ export default function SearchLayout({
     <Container maxWidth="xl">
       <Box
         sx={{
-          display: "flex",
           marginY: 4,
-          flexDirection: {
-            xs: "column",
-            sm: "column",
-            md: "row",
-            lg: "row",
-            xl: "row",
-          },
-          gap: { xs: 1, sm: 1, md: 2, lg: 4, xl: 4 },
         }}
       >
         <Box>{children}</Box>
