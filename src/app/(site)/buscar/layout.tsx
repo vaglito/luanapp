33ec5fca5159b31d4cae1ca2b6eedad1bbb910d5
Cesar Gonzalez <cesargonzalez390@gmail.com>
2025-09-19 03:12:15 -0500

    refactor: :art: remove filter component

--- src/app/(site)/buscar/layout.tsx
+++ src/app/(site)/buscar/layout.tsx
@@ -1,6 +1,4 @@
-import React, { Suspense } from "react";
-import { Filter } from "@/app/components/product/search/Filter";
-import { Box, Container, Typography, Grid2 } from "@mui/material";
+import { Box, Container } from "@mui/material";
 
 export default function SearchLayout({
   children,
@@ -23,29 +21,7 @@ export default function SearchLayout({
           gap: { xs: 1, sm: 1, md: 2, lg: 4, xl: 4 },
         }}
       >
-        <Box
-          sx={{
-            width: { xs: "100%", sm: "100%", md: "20%", lg: "20%", xl: "20%" },
-            backgroundColor: "#fff",
-            boxShadow: "0px 4px 20px rgba(0, 0, 0, 0.1)",
-            borderRadius: "12px",
-          }}
-        >
-          <Suspense
-            fallback={<Typography variant="h6">Cargando filtros...</Typography>}
-          >
-            <Filter />
-          </Suspense>
-        </Box>
-        <Box
-          sx={{
-            width: { xs: "100%", sm: "100%", md: "80%", lg: "80%", xl: "80%" },
-            backgroundColor: "#fff",
-            borderRadius: "12px",
-          }}
-        >
-          {children}
-        </Box>
+        <Box>{children}</Box>
       </Box>
     </Container>
   );
