ab0ff365bcd13c2bb108d7fdd6f4a840ecc3ba44
Cesar Gonzalez <cesargonzalez390@gmail.com>
2025-09-21 06:54:52 -0500

    feat: add ResponseCategories and CategoriesType

--- src/app/types/v2/categorys-type.ts
+++ src/app/types/v2/categorys-type.ts
@@ -1,26 +1,53 @@
-export interface Soplinea {
+export interface ResponseCategories {
+  count: number;
+  next: string;
+  previous: string;
+  results: CategoriesType[];
+}
+
+export interface CategoriesType {
+  id: number;
+  soplinea: {
     cod_line: string;
     nom_line: string;
-}
-
-export interface Sopsub1 {
-    cod_sub1: string;
-    nom_sub1: string;
-}
-
-export interface Subcategory {
+  };
+  slug: string;
+  is_active: boolean;
+  subcategories: {
     pk: number;
     is_active: boolean;
     image?: string;
     slug: string;
-    sopsub1: Sopsub1;
+    sopsub1: {
+      cod_sub1: string;
+      nom_sub1: string;
+    };
+  }[];
+}
+
+export interface Soplinea {
+  cod_line: string;
+  nom_line: string;
+}
+
+export interface Sopsub1 {
+  cod_sub1: string;
+  nom_sub1: string;
+}
+
+export interface Subcategory {
+  pk: number;
+  is_active: boolean;
+  image?: string;
+  slug: string;
+  sopsub1: Sopsub1;
 }
 
 export interface Categorys {
-    pk: number;
-    is_active: boolean;
-    image: string;
-    slug: string;
-    soplinea: Soplinea;
-    subcategory: Subcategory[];
+  pk: number;
+  is_active: boolean;
+  image: string;
+  slug: string;
+  soplinea: Soplinea;
+  subcategory: Subcategory[];
 }
