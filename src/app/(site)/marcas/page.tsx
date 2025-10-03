666cf5bc1a1173ca0b34a8e4eeb2915771842fe0
Cesar Gonzalez <cesargonzalez390@gmail.com>
2025-10-03 16:10:26 -0500

    refactor: add gridBrand component

--- src/app/(site)/marcas/page.tsx
+++ src/app/(site)/marcas/page.tsx
@@ -1,7 +1,6 @@
 import { Container, Box, Typography, Grid2 } from "@mui/material";
 import { getListBrands } from "@/app/utils/brands";
-import Image from "next/image";
-import Link from "next/link";
+import { GridBrand } from "@/app/components/brand/grid-brand";
 
 export const metadata = {
   title: "Marcas | Corporacion Luana",
@@ -13,52 +12,9 @@ export default async function BrandPage() {
 
   return (
     <Container maxWidth="xl">
-      <Box marginY={4}>
-        <Box>
-          <Typography
-            variant="h4"
-            sx={{
-              fontWeight: "bold",
-            }}
-          >
-            Nuestras marcas
-          </Typography>
-        </Box>
+      <Box>
         <Box marginY={4}>
-          <Grid2 container spacing={2}>
-            {brands.map((brand) => (
-              <Grid2
-                size={{ xs: 6, sm: 4, md: 3, lg: 2, xl: 2 }}
-                sx={{
-                  boxShadow: 3,
-                  borderRadius: 3,
-                  transition: "transform 0.3s ease",
-                  "&:hover": {
-                    transform: "translateY(-5px)",
-                  },
-                }}
-                key={brand.id}
-              >
-                <Box sx={{ p: 2 }}>
-                  <Link href={`/marcas/${brand.slug}`}>
-                    <Box>
-                      <Image
-                        src={brand.image}
-                        alt={`Imagen de la marca ${brand.sopsub2.nom_sub2}`}
-                        width={0}
-                        height={0}
-                        sizes="100vw"
-                        className="h-full w-full rounded-xl"
-                      />
-                    </Box>
-                    <Typography sx={{ textAlign: "center" }}>
-                      {brand.sopsub2.nom_sub2}
-                    </Typography>
-                  </Link>
-                </Box>
-              </Grid2>
-            ))}
-          </Grid2>
+          <GridBrand brands={brands} />
         </Box>
       </Box>
     </Container>
