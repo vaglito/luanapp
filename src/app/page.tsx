8dce05ab632a5742a6822c0074c62d815139240e
Cesar Gonzalez <cesargonzalez390@gmail.com>
2025-10-14 11:16:41 -0500

    feat: add generatemetada and change order compoenets

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,9 +11,49 @@ import { ComputerHome } from "./components/computer/computer-home";
 
 export const revalidate = 0;
 
-export const metadata = {
-  title: "Corporacion Luana",
-  description: "Tienda de corporacion luana",
+export const generateMetadata = () => {
+  const image = "../social.jpg";
+  return {
+    title: "Corporación Luana | Seguridad, confianza e innovación",
+    description:
+      "Bienvenido a Corporación Luana, tu tienda online de confianza en Perú. Descubre laptops con video dedicada e integrada, productos tecnológicos, computadoras gamer y para ingenería, accesorios y más. Envíos rápidos y atención personalizada.",
+    keywords: [
+      "Corporación Luana",
+      "tienda online Perú",
+      "laptops con video dedicada",
+      "laptops con video integrada",
+      "computadora gamer",
+      "computadora para ingeneria",
+      "tecnología Perú",
+      "comprar laptops online",
+      "accesorios tecnológicos",
+      "ecommerce Perú",
+    ],
+    openGraph: {
+      title: "Corporación Luana | Seguridad, confianza e innovación",
+      description:
+        "Explora las últimas novedades en laptops, computadoras y tecnología en Corporación Luana. Compra online con envío rápido y atención personalizada.",
+      url: "https://corporacionluana.pe",
+      siteName: "Corporacion luana",
+      images: [
+        {
+          url: image,
+          width: 1200,
+          height: 630,
+          alt: "Corporación Luana | Seguridad, confianza e innovación",
+        },
+      ],
+      locale: "es_PE",
+      type: "website",
+      twitter: {
+        card: "summary_large_image",
+        title: "Corporación Luana | Seguridad, confianza e innovación",
+        description:
+          "Descubre laptops, accesorios y más en Corporación Luana. Compra online con envío rápido.",
+        images: [image],
+      },
+    },
+  };
 };
 
 export default async function Home() {
@@ -28,13 +68,13 @@ export default async function Home() {
     <>
       <BannerHome banners={banners} />
       <Container maxWidth="xl">
+        <HomeCategory categories={categories} />
+      </Container>
+      <Container maxWidth="xl">
         <NewProducts products={newProduct.results} />
       </Container>
       <ComputerHome />
       <SectionLaptop products={productsLaptop.results} />
-      <Container maxWidth="xl">
-        <HomeCategory categories={categories} />
-      </Container>
     </>
   );
 }
