1d19b6b70bc59b794ba4051e0021e5a4ffb6e7a3
Cesar Gonzalez <cesargonzalez390@gmail.com>
2025-10-27 01:19:21 -0500

    refactor: remove old files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
 import { Container } from "@mui/material";
-import { getNewProductList, getProductList } from "./lib/api/products";
 // Components
 import { NewProducts } from "./components/ui/new-products";
 import { SectionLaptop } from "./components/ui/category-product";
 import { HomeCategory } from "./components/ui/home-category";
-import { getCategoryList } from "./lib/api/categorys";
-import { getBannerList } from "./lib/api/common";
 import { BannerHome } from "./components/ui/banner/BannerHome";
 import { ComputerHome } from "./components/computer/computer-home";
+import { fetchBannerHome } from "./services/siteInfo";
 
 export const revalidate = 0;
 
@@ -57,24 +55,19 @@ export const generateMetadata = () => {
 };
 
 export default async function Home() {
-  const newProduct = await getNewProductList();
-  const productsLaptop = await getProductList({
-    subcategorySlug: ["laptop-cvideo-dedicada", "laptop-c-video-integrada"],
-  });
-  const categories = await getCategoryList();
-  const banners = await getBannerList();
+  const banners = await fetchBannerHome()
 
   return (
     <>
       <BannerHome banners={banners} />
-      <Container maxWidth="xl">
+{/*       <Container maxWidth="xl">
         <HomeCategory categories={categories} />
       </Container>
       <Container maxWidth="xl">
         <NewProducts products={newProduct.results} />
       </Container>
       <SectionLaptop products={productsLaptop.results} />
-      <ComputerHome />
+      <ComputerHome /> */}
     </>
   );
 }
