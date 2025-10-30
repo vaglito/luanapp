d95659d959829077b68f9e43562949f80921dc4c
Cesar Gonzalez <cesargonzalez390@gmail.com>
2025-10-30 18:06:44 -0500

    style: add new style

--- src/app/components/layout/footer/top-footer.tsx
+++ src/app/components/layout/footer/top-footer.tsx
@@ -7,19 +7,19 @@ export function TopFooter() {
   return (
     <Box
       component="section"
-      sx={{ bgcolor: "#f0f0f0" }}
       className="drop-shadow-lg"
     >
       <Container maxWidth="xl">
         <Box sx={{ paddingY: 3 }}>
           <Typography
-            variant="h4"
+            variant="h3"
+            color="#545454"
             sx={{ fontWeight: 600, textAlign: "center" }}
           >
-            Ten la mejor experiencia
+            Vive la mejor experiencia
           </Typography>
-          <Typography variant="body1" sx={{ textAlign: "center" }}>
-            Queremos que tengas el mejor de la experiencias.
+          <Typography color="#545454" sx={{ textAlign: "center", fontSize: 18 }}>
+            Nos comprometemos a ofrecerte soluciones confiables, innovadoras y seguras
           </Typography>
         </Box>
         <Box sx={{ paddingY: 3 }}>
@@ -29,7 +29,7 @@ export function TopFooter() {
                 <Box sx={{ display: "flex", justifyContent: "center" }}>
                   <FcShipped className="text-6xl" />
                 </Box>
-                <Typography sx={{ textAlign: "center", fontWeight: 600 }}>
+                <Typography color="#545454" sx={{ textAlign: "center", fontWeight: 600 }}>
                   Delivery
                 </Typography>
               </Box>
@@ -39,7 +39,7 @@ export function TopFooter() {
                 <Box sx={{ display: "flex", justifyContent: "center" }}>
                   <FcLock className="text-6xl" />
                 </Box>
-                <Typography sx={{ textAlign: "center", fontWeight: 600 }}>
+                <Typography color="#545454" sx={{ textAlign: "center", fontWeight: 600 }}>
                   Compra facil y seguro
                 </Typography>
               </Box>
@@ -49,7 +49,7 @@ export function TopFooter() {
                 <Box sx={{ display: "flex", justifyContent: "center" }}>
                   <FcShop className="text-6xl" />
                 </Box>
-                <Typography sx={{ textAlign: "center", fontWeight: 600 }}>
+                <Typography color="#545454" sx={{ textAlign: "center", fontWeight: 600 }}>
                   Retiro gratis en tienda
                 </Typography>
               </Box>
@@ -59,7 +59,7 @@ export function TopFooter() {
                 <Box sx={{ display: "flex", justifyContent: "center" }}>
                   <FcAssistant className="text-6xl" />
                 </Box>
-                <Typography sx={{ textAlign: "center", fontWeight: 600 }}>
+                <Typography color="#545454" sx={{ textAlign: "center", fontWeight: 600 }}>
                   Contactanos
                 </Typography>
               </Box>
