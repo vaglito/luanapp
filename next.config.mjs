c06cbf15b5ac1f673eb87a75cd998287a968a003
Cesar Gonzalez <cesargonzalez390@gmail.com>
2025-10-30 14:07:06 -0500

    add new hostname

--- next.config.mjs
+++ next.config.mjs
@@ -13,7 +13,11 @@ const nextConfig = {
             {
                 protocol: 'http',
                 hostname: '127.0.0.1'
-            }
+            },
+            {
+                protocol: 'http',
+                hostname: 'dev.corporacionluana.pe'
+            },
         ]
     },
 };
