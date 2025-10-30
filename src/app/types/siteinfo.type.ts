83ca1068cf94f7c4fb452da75e77a76850af4a36
Cesar Gonzalez <cesargonzalez390@gmail.com>
2025-10-30 16:25:59 -0500

    add SiteInfoMetadata

---
+++ src/app/types/siteinfo.type.ts
@@ -0,0 +1,21 @@
+export interface SiteInfoMetadata {
+    id: number;
+    siteName: string;
+    slogan: string;
+    metaDescription: string;
+    logo: string;
+    favicon: string;
+    email: string;
+    phone?: string;
+    whatsapp?: string;
+    address?: string;
+    schedule?: string;
+    facebook: string;
+    instagram: string;
+    tiktok: string;
+    youtube?: string;
+    aboutUs?: string;
+    privacyUrl?: string;
+    isActive: boolean;
+    updateAt: string;
+}
