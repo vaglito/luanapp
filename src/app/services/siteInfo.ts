ad7e953d7738dd4157a6738235666199d7eefc8a
Cesar Gonzalez <cesargonzalez390@gmail.com>
2025-10-30 16:25:46 -0500

    feat: add fetchsitemetadata

--- src/app/services/siteInfo.ts
+++ src/app/services/siteInfo.ts
@@ -1,5 +1,6 @@
 import apiClient from "./apiClient";
 import { Banner } from "../types/banner.type";
+import { SiteInfoMetadata } from "../types/siteinfo.type";
 
 export async function fetchBannerHome(): Promise<Banner[]> {
   try {
@@ -7,5 +8,14 @@ export async function fetchBannerHome(): Promise<Banner[]> {
     return response.data;
   } catch (error) {
     throw new Error("Failed to fetch banners");
+  }
+}
+
+export async function fetchSiteMetadata(id: number): Promise<SiteInfoMetadata> {
+  try {
+    const response = await apiClient.get(`/api/site/site-info/${id}/`);
+    return response.data;
+  } catch (error) {
+    throw new Error("Failed to fetch sitemetadata");
   }
 }
