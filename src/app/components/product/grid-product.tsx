1bfd78d993b67308cc1dff5e0b70b3fc5dcb90ab
Cesar Gonzalez <cesargonzalez390@gmail.com>
2025-10-30 12:45:31 -0500

    refactor: grid component

--- src/app/components/product/grid-product.tsx
+++ src/app/components/product/grid-product.tsx
@@ -1,14 +1,19 @@
 import { Grid2 } from "@mui/material";
 import { CardProduct } from "@/app/components/product/card-product";
-import { Product } from "@/app/types/v2/products-type";
+import { Products } from "@/app/types/products.type";
 
-
-export const GridProduct = ({ products }: { products: Product[] }) => {
+export const GridProduct = ({
+  products,
+  exchange,
+}: {
+  products: Products[];
+  exchange: number;
+}) => {
   return (
     <Grid2 container spacing={2}>
       {products.map((product) => (
-        <Grid2 size={{ xs: 6, sm: 4, md: 4, lg: 3, xl: 3 }} key={product.pk} >
-          <CardProduct product={product} />
+        <Grid2 size={{ xs: 6, sm: 4, md: 4, lg: 3, xl: 3 }} key={product.id}>
+          <CardProduct product={product} exchange={exchange}/>
         </Grid2>
       ))}
     </Grid2>
