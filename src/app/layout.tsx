73e5e8f3dd0030423906494d8e720dcc0e676ab6
Cesar Gonzalez <cesargonzalez390@gmail.com>
2025-10-30 16:25:27 -0500

    feat: add generateMetadata and header props

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,10 +3,11 @@ import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
 import { Roboto } from "next/font/google";
 import { Header } from "./components/layout/header/header";
 import { Footer } from "./components/layout/footer/footer";
-import { ThemeProvider } from '@mui/material/styles';
+import { ThemeProvider } from "@mui/material/styles";
 import { themeOptions } from "../../theme";
 import "swiper/css";
 import "./globals.css";
+import { fetchSiteMetadata } from "./services/siteInfo";
 
 const roboto = Roboto({
   weight: ["400", "500", "700"],
@@ -14,26 +15,39 @@ const roboto = Roboto({
   display: "swap",
 });
 
-export const metadata: Metadata = {
-  title: "Corporacion luana",
-  description:
-    "Corporacion Luana lo mejor en computo y accesorios con las mejores marcas.",
-};
+export async function generateMetadata(): Promise<Metadata> {
+  const site = await fetchSiteMetadata(1);
 
-export default function RootLayout({
+  return {
+    title: `${site.siteName} | ${site.slogan}`,
+    description: site.metaDescription,
+    keywords: ["computadoras", "accesorios", "envío rápido"],
+    openGraph: {
+      title: `${site.siteName} | ${site.slogan}`,
+      description: site.metaDescription,
+      images: [site.logo],
+    },
+    icons: {
+      icon: site.favicon
+    }
+  };
+}
+
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const site = await fetchSiteMetadata(1)
   return (
     <html lang="es">
       <body className={`${roboto.className} antialiased`}>
-      <ThemeProvider theme={themeOptions}>
-        <AppRouterCacheProvider options={{ key: "css" }}>
-          <Header />
-          {children}
-          <Footer />
-        </AppRouterCacheProvider>
+        <ThemeProvider theme={themeOptions}>
+          <AppRouterCacheProvider options={{ key: "css" }}>
+            <Header logo={site.logo}/>
+            {children}
+            <Footer />
+          </AppRouterCacheProvider>
         </ThemeProvider>
       </body>
     </html>
