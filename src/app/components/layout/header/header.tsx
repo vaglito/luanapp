bdb7a433b18521ec3a11ba59333ac611caf371cb
Cesar Gonzalez <cesargonzalez390@gmail.com>
2025-10-30 14:14:26 -0500

    style: add backgroundcolor white

--- src/app/components/layout/header/header.tsx
+++ src/app/components/layout/header/header.tsx
@@ -10,66 +10,64 @@ import { Search } from "../search";
 
 export function Header() {
   return (
-    <>
-      <header>
-        <Container maxWidth="xl">
-          <Box
-            sx={{
-              display: "flex",
-              flexDirection: { xs: "column", md: "row" },
-              marginY: 2,
-              alignItems: "center",
-            }}
-          >
-            <Box sx={{ width: { sm: "100%", xs: "100%", md: "30%" } }}>
-              <Link href="/">
-                <Image
-                  src="/logo-corporacion-luana.jpg"
-                  width={0}
-                  height={0}
-                  alt="Logo de corporacion luana"
-                  sizes="100vw"
-                  style={{ width: "100%", height: "auto" }}
-                  priority={true}
-                />
-              </Link>
-            </Box>
-            <Box sx={{ flexGrow: 1, marginY: 1 }}>
-              <Box
-                sx={{
-                  display: "flex", // flex en todas las pantallas para permitir centrado
-                  flexDirection: { xs: "column", md: "row" }, // En xs, los elementos se apilan verticalmente, en md, se alinean horizontalmente
-                  justifyContent: "center", // Centra el contenido horizontalmente
-                  width: "100%",
-                }}
-              >
-                <Suspense fallback={<div>Loading search...</div>}>
-                  <Search />
-                </Suspense>
-              </Box>
-            </Box>
-            <Box>
-              <Box sx={{ display: "flex", gap: 4 }}>
-                <IconButton
-                  color="primary"
-                  aria-label="agrega al carrito de compras"
-                >
-                  <Badge badgeContent={0} color="secondary">
-                    <ShoppingCartIcon />
-                  </Badge>
-                </IconButton>
-                <Button variant="outlined" startIcon={<LoginIcon />}>
-                  Iniciar sesion
-                </Button>
-                <Button variant="contained" startIcon={<PersonIcon />}>
-                  Crear Cuenta
-                </Button>
-              </Box>
+    <Box component="header" sx={{ backgroundColor: "white" }}>
+      <Container maxWidth="xl">
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: { xs: "column", md: "row" },
+            paddingY: 2,
+            alignItems: "center",
+          }}
+        >
+          <Box sx={{ width: { sm: "100%", xs: "100%", md: "30%" } }}>
+            <Link href="/">
+              <Image
+                src="/logo-corporacion-luana.jpg"
+                width={0}
+                height={0}
+                alt="Logo de corporacion luana"
+                sizes="100vw"
+                style={{ width: "100%", height: "auto" }}
+                priority={true}
+              />
+            </Link>
+          </Box>
+          <Box sx={{ flexGrow: 1, marginY: 1 }}>
+            <Box
+              sx={{
+                display: "flex", // flex en todas las pantallas para permitir centrado
+                flexDirection: { xs: "column", md: "row" }, // En xs, los elementos se apilan verticalmente, en md, se alinean horizontalmente
+                justifyContent: "center", // Centra el contenido horizontalmente
+                width: "100%",
+              }}
+            >
+              <Suspense fallback={<div>Loading search...</div>}>
+                <Search />
+              </Suspense>
             </Box>
           </Box>
-        </Container>
-      </header>
+          <Box>
+            <Box sx={{ display: "flex", gap: 4 }}>
+              <IconButton
+                color="primary"
+                aria-label="agrega al carrito de compras"
+              >
+                <Badge badgeContent={0} color="secondary">
+                  <ShoppingCartIcon />
+                </Badge>
+              </IconButton>
+              <Button variant="outlined" startIcon={<LoginIcon />}>
+                Iniciar sesion
+              </Button>
+              <Button variant="contained" startIcon={<PersonIcon />}>
+                Crear Cuenta
+              </Button>
+            </Box>
+          </Box>
+        </Box>
+      </Container>
       <Navbar />
-    </>
+    </Box>
   );
 }
