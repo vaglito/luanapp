88ba75728d9bdb36197586a2afffeb3f9a842715
Cesar Gonzalez <cesargonzalez390@gmail.com>
2025-10-30 12:42:44 -0500

    feat: add fetchCategoriesSearch

--- src/app/services/categories.ts
+++ src/app/services/categories.ts
@@ -9,3 +9,19 @@ export async function fetchCategories(): Promise<Categories[]> {
     throw new Error("Failed to fetch categories");
   }
 }
+
+export async function fetchCategoriesSearch(
+  search: string
+): Promise<Categories[]> {
+  try {
+    const response = await apiClient.get(`/api/categories/categories/search/`, {
+      params: {
+        search: search,
+      },
+      paramsSerializer: { indexes: null },
+    });
+    return response.data;
+  } catch (error) {
+    throw new Error("Failed fetch serach categories.");
+  }
+}
