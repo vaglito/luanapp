c7a458c8bd8b20db4347d65c5edd2212e71a7122
Cesar Gonzalez <cesargonzalez390@gmail.com>
2025-11-15 02:52:45 -0500

    feat: add google analytics and tag manager

---
+++ src/app/components/GTMbody.tsx
@@ -0,0 +1,12 @@
+const GTM_ID = 'GTM-ML742DT5'
+
+export const GTMBody = () => (
+  <noscript>
+    <iframe
+      src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+      height="0"
+      width="0"
+      style={{ display: 'none', visibility: 'hidden' }}
+    />
+  </noscript>
+)
