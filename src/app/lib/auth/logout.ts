b734784206a0c032a6287735e4231d0e36c3c66b
Cesar Gonzalez <cesargonzalez390@gmail.com>
2026-01-07 04:40:03 -0500

    fix: use esrver

--- src/app/lib/auth/logout.ts
+++ src/app/lib/auth/logout.ts
@@ -1,3 +1,4 @@
+"use server";
 import { auth, signOut } from "@/auth";
 import { axiosAuth } from "../axios";
 
--- src/app/lib/auth/refresh-token.ts
+++ src/app/lib/auth/refresh-token.ts
@@ -1,17 +1,18 @@
 import axios from "axios";
 import { JWT } from "next-auth/jwt";
-import { signOut } from "next-auth/react";
+import { signOut } from "@/auth";
 
 interface RefreshResponse {
   access: string;
 }
-const API_URL = process.env.API_URL
-const API_KEY = process.env.API_KEY
+const API_URL = process.env.API_URL;
+const API_KEY = process.env.API_KEY;
+
 export async function refreshAccessToken(token: JWT): Promise<JWT> {
   try {
     const res = await axios.post<RefreshResponse>(
       `${API_URL}/api/v2.0/auth/login/refresh/`,
-      { refresh: token.refreshToken, },
+      { refresh: token.refreshToken },
       {
         headers: {
           "Content-Type": "application/json",
