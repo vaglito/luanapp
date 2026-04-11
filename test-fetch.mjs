0412d45618dd72d919504f1263658758166bc824
Cesar Gonzalez <cesargonzalez390@gmail.com>
2026-04-10 19:14:01 -0500

    refactor: :fire: remove file .md and test_fetch debug

--- test-fetch.mjs
+++
@@ -1,8 +0,0 @@
-import fetch from 'node-fetch';
-
-try {
-  const res = await fetch('https://fonts.googleapis.com/css2?family=Roboto:wght@400&display=swap');
-  console.log('Status:', res.status);
-} catch (err) {
-  console.error('Error:', err);
-}
