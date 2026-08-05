dd929f99659ebd64d366b0e84b0b1ee458827b0f
Cesar Enrique Gonzalez Carvajal <vaglito.dev@gmail.com>
2026-08-05 08:56:33 -0500

    fix: add use client to product detail description to prevent DOMPurify SSR crash

--- src/components/catalog/product/detail/product-detail-description.tsx
+++ src/components/catalog/product/detail/product-detail-description.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Box, Typography } from "@mui/material";
 import { ProductPrice } from "@/components/catalog/product/detail/product-price";
 import { ShopFunction } from "./shop-functions";
